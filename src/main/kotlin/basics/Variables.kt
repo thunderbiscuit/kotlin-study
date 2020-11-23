22d4b9e5f2dc235ab24ec90f70598700ccac2341
thunderbiscuit <thunderB@protonmail.com>
2020-11-23 11:21:36 -0500

    Add variables examples

---
+++ src/main/kotlin/basics/Variables.kt
@@ -0,0 +1,18 @@
+package basics
+
+// variables are declared as being immutable (val) or mutable (var)
+// the variable type is declared after a colon right after the name of the variable
+
+val name: String = "thunderbiscuit"
+var age: Int = 30
+
+// variable type can be inferred by the compiler
+val country = "Canada"
+
+fun printInfo(name: String, age: Int) {
+    println("Name: $name, Age: $age")
+}
+
+// variables by default are non-nullable
+//you can allow a variable to be null by declaring it like so:
+var kids: Boolean? = null
