998e4664e8abada131ca4a812c330e9476c24b8b
thunderbiscuit <thunderB@protonmail.com>
2020-11-23 21:11:54 -0500

    Add default imports to imports

--- src/main/kotlin/standardlibrary/Imports.kt
+++ src/main/kotlin/standardlibrary/Imports.kt
@@ -1,6 +1,21 @@
 // each file declares its belonging to a package with the following statement
 // it is customary to have all files in the same directory belong to the same package but it does not have to be so
-package standardLibrary
+package standardlibrary
+
+
+// a number of packages are imported into every Kotlin file by default
+// import kotlin.*
+// import kotlin.annotation.*
+// import kotlin.collections.*
+// import kotlin.comparisons.*
+// import kotlin.io.*
+// import kotlin.ranges.*
+// import kotlin.sequences.*
+// import kotlin.text.*
+
+// in the case of Kotlin on the JVM, those packages are also imported by default
+// import java.lang.*
+// import kotlin.jvm.*
 
 // we import from the Standard Library using statements like the following
 // we can rename symbols in their import statements
