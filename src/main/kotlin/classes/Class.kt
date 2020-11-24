319bc3cfae94db1a4727a91d1904d99dfe2f534c
thunderbiscuit <thunderB@protonmail.com>
2020-11-23 21:11:01 -0500

    Add class examples

---
+++ src/main/kotlin/classes/Class.kt
@@ -0,0 +1,100 @@
+package classes
+
+// class is declared with its primary constructor directly in the class header (first line)
+class Student constructor(age: Int, school: String,) {
+
+    // the public visibility modifier is used by default, meaning the variable is visible everywhere
+    val currentSchool: String = school
+
+    // a private variable will not be accessible outside of the class
+    // a protected variable will only be visible to subclasses
+    private var age: Int = age
+
+    fun printSchool(): Unit {
+        println("Student goes to: $currentSchool")
+    }
+}
+
+
+
+// if the primary constructor does not have any annotations or visibility modifiers
+// the constructor keyword can be omitted
+class Dog(age: Int) {
+    var age: Int = age
+    val cute: Boolean = true
+}
+
+
+
+// if there are no parameters needed in the primary constructor, they do not need to be there
+class Honeybadger {
+    val dontcare: Boolean = true
+}
+
+
+
+// classes can also declare secondary constructors inside the class declaration
+class Puffin constructor(age: Int) {
+    var ageOfPuffin: Int = age
+
+    constructor() : this(age = 100) {
+        println("Secondary constructor was used")
+    }
+
+    fun printAge(): Unit {
+        println("This puffin is $ageOfPuffin")
+    }
+}
+
+
+
+// Kotlin creates getters and setters automatically for us
+// but we can declare specific getters and setters
+class Bison constructor(age: Int) {
+    var ageOfBison: Int = age
+        get() {
+            println("The getter on this bison's age was run")
+            return field - 7
+        }
+        set(value) {
+            println("A trickster has set the new age of this bison")
+            field = value + 100
+        }
+}
+
+
+
+// Kotlin can declare the properties for us if we declare the parameters as val or var in the class header
+// those properties will have their getters and setters set by default
+class Lion constructor(var age: Int, val name: String) {
+    fun printInfo() {
+        println("$name the lion is $age years old")
+    }
+}
+
+
+
+fun main() {
+    val matt: Student = Student(age = 27, school = "Stanford")
+    matt.printSchool()
+    println(matt.currentSchool)
+    // println(matt.age) // throws error because age is private to the Student class
+
+    val bitcoin: Honeybadger = Honeybadger()
+    println(bitcoin.dontcare)
+
+    // create objects using the primary and secondary constructors
+    val henry: Puffin = Puffin(age = 25)
+    henry.printAge()
+    val arty: Puffin = Puffin()
+    arty.printAge()
+
+    // custom getters and setters
+    val marty: Bison = Bison(30)
+    println(marty.ageOfBison)
+    marty.ageOfBison = 24
+    println(marty.ageOfBison)
+
+    val sam: Lion = Lion(age = 7, name = "Sam",)
+    sam.printInfo()
+}
